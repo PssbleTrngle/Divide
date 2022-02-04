d4713252e2167e54394fea27e36b8cc4c2455632
PssbleTrngle <nik@somethingcatchy.net>
2022-02-04 09:31:02 +0100

    database log

--- web/server/database.ts
+++ web/server/database.ts
@@ -1,7 +1,9 @@
-import { MongoClient } from "./deps.ts"
+import { MongoClient, colors } from "./deps.ts"
 
 const client = new MongoClient()
 
-await client.connect(Deno.env.get("MONGO_URI") || "mongodb://localhost:27017")
+const uri = Deno.env.get("MONGO_URI") || "mongodb://localhost:27017"
+console.log(`Connecting to database at ${colors.underline(uri)}`)
+await client.connect(uri)
 
 export default client.database(Deno.env.get("MONGO_DB") || "divide")
