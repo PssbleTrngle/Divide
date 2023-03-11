32226c992cebc470d8c617198223a5ddde334090
Niklas Widmann <niklas.widmann@zuehlke.com>
2023-03-11 12:57:23 +0100

    polymorphic bounty

--- src/main/kotlin/possible_triangle/divide/bounty/Amount.kt
+++ src/main/kotlin/possible_triangle/divide/bounty/Amount.kt
@@ -1,30 +1,44 @@
 package possible_triangle.divide.bounty
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlin.math.max
 import kotlin.math.min
 
 @Serializable
-data class Amount(private val type: Type, private val values: List<Int>) {
+sealed class Amount {
+    abstract fun get(index: Int): Int
+}
 
-    @Serializable
-    enum class Type {
-        CONSTANT,
-        INCREASING,
-        DECREASING,
-        FIRST,
+@Serializable
+@SerialName("constant")
+data class ConstantAmount(val value: Int) : Amount() {
+    override fun get(index: Int) = value
+}
+
+@Serializable
+@SerialName("increasing")
+data class IncreasingAmount(val base: Int, val step: Int, val max: Int? = null) : Amount() {
+    override fun get(index: Int): Int {
+        val amount = base + step * index
+        return if (max == null) amount
+        else min(amount, max)
     }
+}
 
-    fun get(index: Int): Int {
-        return when (type) {
-            Type.CONSTANT -> values.first()
-            Type.INCREASING -> min(values.first() + values.getOrElse(1) { 0 } * index, values.getOrElse(2) { Int.MAX_VALUE })
-            Type.DECREASING -> max(values.first() - values.getOrElse(1) { 0 } * index, values.getOrElse(2) { 0 })
-            Type.FIRST ->
-                if (index <= 0) values.first()
-                else if (values.size < 3 || index <= values[2]) values.getOrElse(1) { 0 }
-                else 0
-        }
+@Serializable
+@SerialName("decreasing")
+data class DecreasingAmount(val base: Int, val step: Int, val min: Int = 0) : Amount() {
+    override fun get(index: Int): Int {
+        val amount = base - step * index
+        return max(amount, min)
     }
+}
 
+@Serializable
+@SerialName("list")
+data class ListAmount(val values: List<Int>) : Amount() {
+    override fun get(index: Int): Int {
+        return values.getOrElse(index) { 0 }
+    }
 }
--- src/main/kotlin/possible_triangle/divide/bounty/Bounty.kt
+++ src/main/kotlin/possible_triangle/divide/bounty/Bounty.kt
@@ -8,7 +8,6 @@ import net.minecraft.server.MinecraftServer
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.scores.PlayerTeam
-import possible_triangle.divide.bounty.Amount.Type.*
 import possible_triangle.divide.data.DefaultedResource
 import possible_triangle.divide.data.EventTarget
 import possible_triangle.divide.data.PerTeamIntData
@@ -48,36 +47,36 @@ data class Bounty(val description: String, val amount: Amount) {
             entry.id = id
         }
 
-        val PLAYER_KILL by defaulted("PLAYER_KILL") { Bounty("Kill a Player", Amount(CONSTANT, listOf(100))) }
-        val BLOWN_UP by defaulted("BLOWN_UP") { Bounty("Blow a player up", Amount(CONSTANT, listOf(200))) }
+        val PLAYER_KILL by defaulted("PLAYER_KILL") { Bounty("Kill a Player", ConstantAmount(20)) }
+        val BLOWN_UP by defaulted("BLOWN_UP") { Bounty("Blow a player up", ConstantAmount(60)) }
         val ADVANCEMENT by defaulted("ADVANCEMENT") {
             Bounty(
                 "Unlock an advancement",
-                Amount(INCREASING, listOf(10, 5, 150))
+                IncreasingAmount(10, 5, 150)
             )
         }
 
-        val SOLD_HEART by defaulted("SOLD_HEART") { Bounty("Sold a heart", Amount(INCREASING, listOf(100, 20))) }
+        val SOLD_HEART by defaulted("SOLD_HEART") { Bounty("Sold a heart", IncreasingAmount(100, 20)) }
 
-        val MINED_COAL by defaulted("MINED_COAL") { Bounty("Mine a coal ore", Amount(DECREASING, listOf(10, 1))) }
-        val MINED_IRON by defaulted("MINED_IRON") { Bounty("Mine a iron ore", Amount(DECREASING, listOf(20, 2))) }
-        val MINED_GOLD by defaulted("MINED_GOLD") { Bounty("Mine a gold ore", Amount(DECREASING, listOf(25, 1))) }
+        val MINED_COAL by defaulted("MINED_COAL") { Bounty("Mine a coal ore", DecreasingAmount(10, 1)) }
+        val MINED_IRON by defaulted("MINED_IRON") { Bounty("Mine a iron ore", DecreasingAmount(20, 2)) }
+        val MINED_GOLD by defaulted("MINED_GOLD") { Bounty("Mine a gold ore", DecreasingAmount(25, 1)) }
         val MINED_DIAMOND by defaulted("MINED_DIAMOND") {
             Bounty(
                 "Mine a diamond ore",
-                Amount(DECREASING, listOf(160, 20))
-            )
+                    DecreasingAmount(160, 20)
+                )
         }
         val MINED_EMERALD by defaulted("MINED_EMERALD") {
             Bounty(
                 "Mine a emerald ore",
-                Amount(DECREASING, listOf(160, 20))
+                DecreasingAmount(160, 20)
             )
         }
         val MINED_NETHERITE by defaulted("MINED_NETHERITE") {
             Bounty(
                 "Mine ancient debris",
-                Amount(DECREASING, listOf(300, 20))
+                DecreasingAmount(300, 20)
             )
         }
 
